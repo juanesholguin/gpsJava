1fcf0ffae3835bf2b22fe93231dccf18b14391bf
juan.holguin <juan.holguin@ariadnacg.com>
2022-04-05 20:47:40 -0500

    change text

--- src/main/java/edu/ucaldas/gps/GpsApplication.java
+++ src/main/java/edu/ucaldas/gps/GpsApplication.java
@@ -15,7 +15,7 @@ public class GpsApplication {
 	}
 
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "willy3") String name) {
+	public String hello(@RequestParam(value = "name", defaultValue = "willy4") String name) {
 		return String.format("Hello %s!", name);
 	}
 }
